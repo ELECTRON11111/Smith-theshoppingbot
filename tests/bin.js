6874b348bd45a55a399541a4a6973ba17829a315
ELECTRON11111 <68711380+ELECTRON11111@users.noreply.github.com>
2022-07-18 12:42:20 +0100

    Base Code

---
+++ tests/bin.js
@@ -0,0 +1,11 @@
+/** const nameOfGood = document.getElementById('good').value;
+        const quantity = document.getElementById('quantity').value;
+        nameOfGood = '';
+        quantity = '';**/
+        /**let myArray = [];
+        for(const stock in stocks) {
+            if(stocks.hasOwnProperty(stock)){
+                myArray.push(stock);
+            }
+        }**/
+       
